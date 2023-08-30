4e2c1c88e9badbf0ab1d726818b7e7751cdaefa5
CantarinoG <guilhermecantarino1709@gmail.com>
2023-08-30 13:55:22 -0300

    Add project structure

---
+++ DeterministicFiniteAutomaton/src/main/java/cantarino/deterministicfiniteautomaton/DeterministicFiniteAutomaton.java
@@ -0,0 +1,8 @@
+package cantarino.deterministicfiniteautomaton;
+
+public class DeterministicFiniteAutomaton {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
